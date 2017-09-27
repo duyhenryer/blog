97bf23db7884dc270d0758aae3fd84736e77e3c7
duyhenryer <duyhenry250897@gmail.com>
2017-09-27 15:50:00 +0700

    urlpatterns = [...]

--- blog/urls.py
+++ blog/urls.py
@@ -19,9 +19,16 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+from accounts.views import (register_view, login_view, logout_view )
+
 urlpatterns = [
-    url(r'^posts/', include('posts.urls', namespace="posts")),
     url(r'^admin/', admin.site.urls),
+    # url(r'^comments/', include("comments.urls", namespace='comments')),
+    url(r'^register/', register_view, name='register'),
+    url(r'^login/', login_view, name='login'),
+    url(r'^logout/', logout_view, name='logout'),
+    url(r'^', include('posts.urls', namespace="posts")),
+
 ]
 
 if settings.DEBUG:
