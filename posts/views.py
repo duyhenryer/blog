9c3e2ceb60cbc60c17046220a4cf77abfcfa1619
duyhenryer <duyhenry250897@gmail.com>
2017-10-06 23:14:46 +0700

    share social media

--- posts/views.py
+++ posts/views.py
@@ -2,10 +2,16 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.contrib import messages
+from django.contrib.contenttypes.models import ContentType
 from urllib.parse import quote_plus
 from django.db.models import Q
 from .models import Post
 from .forms import PostForm
+from comments.forms import CommentForm
+from comments.models import Comment
+from django.utils import timezone
+
+
 
 def post_create(request):
     if not request.user.is_staff or not request.user.is_superuser:
@@ -16,24 +22,110 @@ def post_create(request):
         instance.user = request.user
         instance.save()
         #messages success
-        messages.success(request, "successfully created")
-        return HttpResponseRedirect(instance.get_absolute_url())
+        return render(request, "post_list.html")
+        # messages.success(request,"scusses ")
+        # return HttpResponseRedirect(instance.get_absolute_url())
     context = {
             "form": form,
     }
     return render(request, "post_form.html", context)
 
 def post_detail(request,slug=None):
+
     instance = get_object_or_404(Post, slug=slug)
     share_string = quote_plus(instance.content)
+    initial_data = {
+            "content_type": instance.get_content_type,
+            "object_id": instance.id,
+        }
+
+    form = CommentForm(request.POST or None, initial=initial_data)
+    if form.is_valid() and request.user.is_authenticated():
+
+        # new_comment = form.save(commit=False)
+        # new_comment.post = Post
+        # new_comment.save()
+
+        c_type = form.cleaned_data.get("content_type")
+        content_type = ContentType.objects.get(model=c_type)
+        obj_id = form.cleaned_data.get('object_id')
+        content_data = form.cleaned_data.get("content")
+        # new_comment, created = Comment.objects.get_or_create(
+        #     user=request.user,
+        #     content_type=content_type,
+        #     object_id=obj_id,
+        #     content=content_data,
+        # )
+
+        content = {
+            "content_type": content_type,
+            "obj_id": obj_id,
+            "content_data": content_data,
+        }
+
+        # return HttpResponseRedirect(content_object.get_absolute_url())
+        return render(request, content)
+    # comments = instance.comments
+
+
     context = {
         "title": instance.title,
         "instance": instance,
         "share_string": share_string,
+        "comment_form": form,
     }
+
+
     return render(request, "post_detail.html", context)
 
+
+#
+# def post_detail(request,slug=None):
+#     instance = get_object_or_404(Post, slug=slug)
+#     share_string = quote_plus(instance.content)
+#     # content_type = ContentType.objects.ge
+# t_for_model(Post)
+#     # obj_id = instance.id
+#     comments = Comment.objects.filter_by_instance(instance)
+#
+#     initial_data = {
+#             "content_type": instance.get_content_type,
+#             "object_id": instance.id,
+#         }
+#     form = CommentForm(request.POST or None, initial=initial_data)
+#     if form.is_valid():
+#         print(form.cleaned_data)
+#
+#         c_type = form.cleaned_data.get("content_type")
+#         content_type = ContentType.objects.get(model=c_type)
+#         obj_id = form.cleaned_data.get("object_id")
+#         content_data = form.cleaned_data.get("content")
+#         new_comment, created = Comment.objects.get_or_create(
+#             user=request.user,
+#             content_type=content_type,
+#             object_id=obj_id,
+#             content=content_data,
+#         )
+#         #
+#         # if created:
+#         #     print("Yeeeee....")
+#
+#     comments = instance.comments
+#     context = {
+#         "title": instance.title,
+#         "instance": instance,
+#         "share_string": share_string,
+#         "comments": comments,
+#         "comment_form": form,
+#
+#     }
+#
+#     return render(request, "post_detail.html", context)
+#
+#
+
 def post_list(request):
+    today = timezone.now().date()
     queryset_list = Post.objects.all()  #.all()  #.order_by("-timestamp")
 
     query = request.GET.get("q")
@@ -55,8 +147,9 @@ def post_list(request):
         queryset = paginator.page(paginator.num_pages)
     content = {
         "object_list": queryset,
-        "title": "ExplorePic",
-        "page_request_var": page_request_var
+        # "title": "ExplorePic",
+        "page_request_var": page_request_var,
+        "today": today,
     }
     return render(request, "post_list.html", content)
 
