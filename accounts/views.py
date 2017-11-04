28839ed73b7f2192eb8add1e50730228654e486d
duyhenryer <duyhenry250897@gmail.com>
2017-11-04 14:06:41 +0700

    account

--- accounts/views.py
+++ accounts/views.py
@@ -1,49 +1,60 @@
-from django.shortcuts import render, redirect
-
 from django.contrib.auth import (
-authenticate,
-get_user_model,
-login,
-logout
-)
-from accounts.form import UserLoginForm, UserRegisterForm
+    authenticate,
+    get_user_model,
+    login,
+    logout,
+
+    )
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import UserLoginForm, UserRegisterForm
+
+from django.views.generic.base import TemplateView
+
 def login_view(request):
-    # print(request.user.is_authenticate())
-    next = request.GET.get("next")
-    title = "Login"
+    print(request.user.is_authenticated())
+    next = request.GET.get('next')
+    title = "Want to join?"
     form = UserLoginForm(request.POST or None)
     if form.is_valid():
         username = form.cleaned_data.get("username")
-        password = form.cleaned_data.get("password")
+        password = form.cleaned_data.get('password')
         user = authenticate(username=username, password=password)
-
         login(request, user)
         if next:
             return redirect(next)
         return redirect("/")
-    return render(request, "form.html", {"form": form, "title": title})
+    return render(request, "login.html", {"form":form, "title": title})
+
 
 def register_view(request):
-    # print(request.user.is_authenticate())
-    title = "Register"
-    next = request.GET.get("next")
+    print(request.user.is_authenticated())
+    next = request.GET.get('next')
+    title = "Want to join?"
     form = UserRegisterForm(request.POST or None)
     if form.is_valid():
         user = form.save(commit=False)
-        password = form.cleaned_data.get("password")
+        password = form.cleaned_data.get('password')
         user.set_password(password)
         user.save()
-        new_user = authenticate(user=user.username, password=password)
+        new_user = authenticate(username=user.username, password=password)
         login(request, new_user)
         if next:
             return redirect(next)
         return redirect("/")
 
-    content = {
+    context = {
         "form": form,
-        "title": title,
+        "title": title
     }
-    return render(request, "form.html", content)
+    return render(request, "register.html", context)
+
+
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
 
 def logout_view(request):
     logout(request)
