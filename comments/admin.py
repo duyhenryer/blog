6b6223574d886054446fa8a7e5eb6948cb6dcac1
duyhenryer <duyhenry250897@gmail.com>
2017-11-04 14:07:16 +0700

    update comment

--- comments/admin.py
+++ comments/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Comment
 # Register your models here.
+
+from .models import Comment
 
 admin.site.register(Comment)
--- comments/forms.py
+++ comments/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 
+
+
 class CommentForm(forms.Form):
     content_type = forms.CharField(widget=forms.HiddenInput)
     object_id = forms.IntegerField(widget=forms.HiddenInput)
-    content = forms.CharField(widget=forms.Textarea)
-
+    #parent_id = forms.IntegerField(widget=forms.HiddenInput, required=False)
+    content = forms.CharField(label='', widget=forms.Textarea)
