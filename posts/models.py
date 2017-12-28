43437234d77bb013305d2c28383747e646069278
D-Booker <duyhenry250897@gmail.com>
2017-12-28 23:26:37 +0700

    finally

--- posts/models.py
+++ posts/models.py
@@ -1,53 +1,76 @@
+# from __future__ import unicode_literals
+
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import pre_save
-from django.utils.text import slugify
-# from comments.models import Comment
 from django.utils import timezone
+from django.utils.safestring import mark_safe
 from django.utils.text import slugify
 
+
+from markdown_deux import markdown
+from comments.models import Comment
+
+from .utils import get_read_time
+# Create your models here.
+# MVC MODEL VIEW CONTROLLER
+
+
+#Post.objects.all()
+#Post.objects.create(user=user, title="Some time")
+
+# class PostManager(models.Manager):
+#     def active(self, *args, **kwargs):
+#         # Post.objects.all() = super(PostManager, self).all()
+#         return super(PostManager, self).filter(draft=False).filter(publish__lte=timezone.now())
 
 class PostManager(models.Manager):
     def active(self, *args, **kwargs):
         # Post.objects.all() = super(PostManager, self).all()
-        return super(PostManager, self).filter(draft=False).filter(publish__lte=timezone.now())
+        return super(PostManager, self).filter(id=False).filter(publish__lte=timezone.now())
+
 
 def upload_location(instance, filename):
-    # filebase, extension = filename.split(".")
     return "%s/%s" %(instance.id, filename)
-    # PostModel = instance.__class__
-    # new_id = PostModel.objects.order_by("id").last().id + 1
-
-    # return "%s/%s" %(new_id, filename)
 
 class Post(models.Model):
-    # user
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
-    title = models.CharField(max_length=120)
+    title = models.CharField(max_length=30)
     slug = models.SlugField(unique=True)
     image = models.ImageField(upload_to=upload_location,
-                              null=True, blank=True,
+                              null=True,
+                              blank=True,
                               width_field="width_field",
-                              height_field="height_field")
+                              height_field="height_field"
+                              )
+
+
     height_field = models.IntegerField(default=0)
     width_field = models.IntegerField(default=0)
     content = models.TextField()
+    draft = models.BooleanField(default=False)
+    publish = models.DateField(auto_now=False, auto_now_add=False)
+    read_time =  models.IntegerField(default=0) # models.TimeField(null=True, blank=True) #assume minutes
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
-    # content = models.TextField()
-    # draft = models.BooleanField(default=False)
-    # publish = models.DateField(auto_now=False, auto_now_add=False)
-    # read_time =  models.IntegerField(default=0) # models.TimeField(null=True, blank=True) #assume minutes
-    # updated = models.DateTimeField(auto_now=True, auto_now_add=False)
-    # timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
-    #
+# class NewLink(models.Model):
+#     upvote = models.IntegerField(default=0)
+#     downvote =  models.IntegerField(default=0)
+#     url = models.URLField(null=True)
+#
+#     def __unicode__(self):
+#         return self.url
+#
+#     def link_likes_sum(self):
+#         return self.upvote + self.downvote
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.publish = None
+
+
+    objects = PostManager()
+
 
     def __unicode__(self):
         return self.title
@@ -55,16 +78,22 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-    # edit  slug #fix id
     def get_absolute_url(self):
         return reverse("posts:detail", kwargs={"slug": self.slug})
 
+    class Meta:
+        ordering = ["-timestamp", "-updated"]
 
-    # @property
-    # def comments(self):
-    #     instance = self
-    #     qs = Comment.objects.filter_by_instance(instance)
-    #     return qs
+    def get_markdown(self):
+        content = self.content
+        markdown_text = markdown(content)
+        return mark_safe(markdown_text)
+
+    @property
+    def comments(self):
+        instance = self
+        qs = Comment.objects.filter_by_instance(instance)
+        return qs
 
     @property
     def get_content_type(self):
@@ -78,14 +107,31 @@ def create_slug(instance, new_slug=None):
     if new_slug is not None:
         slug = new_slug
     qs = Post.objects.filter(slug=slug).order_by("-id")
-    exits = qs.exists()
-    if exits:
+    exists = qs.exists()
+    if exists:
         new_slug = "%s-%s" %(slug, qs.first().id)
         return create_slug(instance, new_slug=new_slug)
     return slug
 
-def pre_save_post_receiver(sender, instance, *args, **kwargs ):
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
     if not instance.slug:
         instance.slug = create_slug(instance)
 
+    if instance.content:
+        html_string = instance.get_markdown()
+        read_time_var = get_read_time(html_string)
+        instance.read_time = read_time_var
+
+
 pre_save.connect(pre_save_post_receiver, sender=Post)
+
+
+
+
+
+
+
+
+
+
